2d9dcf47d98ac1f27b36c9d5f0261a81de6f23ef
Kamil Nowak <it.kamilnowak@gmail.com>
2019-08-19 09:48:48 +0200

    Updates

---
+++ precache-manifest.893c732391390028332b3ba20f5c5e26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0427a53125d96329d3f6191f4e9cd0b0",
+    "url": "/mini-shop/index.html"
+  },
+  {
+    "revision": "5f5d2822fd011b7c3d8e",
+    "url": "/mini-shop/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "f9ec9bc4fe4f43a69fe9",
+    "url": "/mini-shop/static/js/2.063b1d48.chunk.js"
+  },
+  {
+    "revision": "5f5d2822fd011b7c3d8e",
+    "url": "/mini-shop/static/js/main.495f2a9d.chunk.js"
+  },
+  {
+    "revision": "6c7b464dacb5ed57fdbb",
+    "url": "/mini-shop/static/js/runtime~main.51fda3ee.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mini-shop/static/media/logo.5d5d9eef.svg"
+  }
+]);
